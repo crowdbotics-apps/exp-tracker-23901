da462e886a4082783394822d4b79f4352b05ed95
Crowdbotics <team@crowdbotics.com>
2021-01-17 23:38:49 +0000

    Update components

--- src/features/BlankScreen2194005/screens/blank.js
+++ src/features/BlankScreen2194005/screens/blank.js
@@ -21,6 +21,7 @@ import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 class Blank extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {}
   }
   static navigationOptions = ({ navigation }) => {
@@ -29,16 +30,36 @@ class Blank extends React.Component {
 
   render = () => (
     <View>
-      <View />
+      <View style={styles.View_3}>
+        <View style={styles.View_8}>
+          <View style={styles.View_9} />
+          <View style={styles.View_10} />
+        </View>
+      </View>
+      <View style={styles.View_4}>
+        <View />
+        <View />
+      </View>
     </View>
   )
 }
 
-const styles = StyleSheet.create({ View_1: {}, View_3: {} })
+const styles = StyleSheet.create({
+  View_1: {},
+  View_3: { width: 260, height: 200 },
+  View_8: { width: 100, height: 100, flexDirection: "row" },
+  View_9: { width: 100, height: 100 },
+  View_10: { width: 0 },
+  View_4: { flexDirection: "row" },
+  View_5: {},
+  View_6: {}
+})
 const mapStateToProps = state => {
   return {}
 }
+
 const mapDispatchToProps = () => {
   return {}
 }
+
 export default connect(mapStateToProps, mapDispatchToProps)(Blank)
