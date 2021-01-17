0adc4c746ccc241c30b2b2318ccb0ea951310bae
Crowdbotics <team@crowdbotics.com>
2021-01-17 23:42:03 +0000

    Update components

--- src/features/BlankScreen2194005/screens/blank.js
+++ src/features/BlankScreen2194005/screens/blank.js
@@ -30,13 +30,16 @@ class Blank extends React.Component {
 
   render = () => (
     <View>
-      <View style={styles.View_3}>
-        <View style={styles.View_8}>
-          <View style={styles.View_9} />
-          <View style={styles.View_10} />
+      <View style={styles.View_4}>
+        <View style={styles.View_5}>
+          <Image
+            source={{ uri: "https://via.placeholder.com/150" }}
+            style={styles.Image_16}
+          />
+          <Text>Hi Mr X</Text>
         </View>
       </View>
-      <View style={styles.View_4}>
+      <View style={styles.View_11}>
         <View />
         <View />
       </View>
@@ -46,14 +49,15 @@ class Blank extends React.Component {
 
 const styles = StyleSheet.create({
   View_1: {},
-  View_3: { width: 260, height: 200 },
-  View_8: { width: 100, height: 100, flexDirection: "row" },
-  View_9: { width: 100, height: 100 },
-  View_10: { width: 0 },
   View_4: { flexDirection: "row" },
-  View_5: {},
-  View_6: {}
+  View_5: { width: 200, height: 50 },
+  Image_16: { width: 40, height: 30 },
+  Text_20: {},
+  View_11: { flexDirection: "row" },
+  View_12: {},
+  View_13: {}
 })
+
 const mapStateToProps = state => {
   return {}
 }
