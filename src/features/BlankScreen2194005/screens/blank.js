8f8a17914e4554a4b68ca9714556bfdb8207ff17
Crowdbotics <team@crowdbotics.com>
2021-01-17 23:56:44 +0000

    Update components

--- src/features/BlankScreen2194005/screens/blank.js
+++ src/features/BlankScreen2194005/screens/blank.js
@@ -33,7 +33,10 @@ class Blank extends React.Component {
       <View style={styles.View_4}>
         <View style={styles.View_5}>
           <Image
-            source={{ uri: "https://via.placeholder.com/150" }}
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/23901/895fe50f-88c1-4d71-920b-795c0b505070.png"
+            }}
             style={styles.Image_16}
           />
           <Text style={styles.Text_20}>Hi Mr X</Text>
@@ -47,29 +50,45 @@ class Blank extends React.Component {
       <View style={styles.View_35}>
         <View style={styles.View_36}>
           <Image
-            source={{ uri: "https://via.placeholder.com/150" }}
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/23901/a04a9fc6-6c79-48fb-af7b-e120b421fa20.png"
+            }}
             style={styles.Image_45}
           />
+          <Text>Gestion de demande de remboursement </Text>
         </View>
         <View style={styles.View_37}>
           <Image
-            source={{ uri: "https://via.placeholder.com/150" }}
-            style={styles.Image_48}
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/23901/3ca7b91b-50ee-4edf-9340-b1129400a3d0.png"
+            }}
+            style={styles.Image_138}
           />
+          <Text>Tracer les dépences</Text>
         </View>
       </View>
       <View style={styles.View_41}>
         <View style={styles.View_42}>
           <Image
-            source={{ uri: "https://via.placeholder.com/150" }}
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/23901/4fddbf76-ad10-4c31-81ec-40d2964e5280.jpg"
+            }}
             style={styles.Image_50}
           />
+          <Text>calculer les dépences</Text>
         </View>
         <View style={styles.View_43}>
           <Image
-            source={{ uri: "https://via.placeholder.com/150" }}
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/resources/project/23901/55a22aae-1406-45dd-9477-8aac0bf35552.png"
+            }}
             style={styles.Image_53}
           />
+          <Text>Scanner une facture</Text>
         </View>
       </View>
     </View>
@@ -80,7 +99,7 @@ const styles = StyleSheet.create({
   View_1: {},
   View_4: { flexDirection: "row" },
   View_5: { width: 260, height: 50 },
-  Image_16: { width: 40, height: 30 },
+  Image_16: { width: 50, height: 50 },
   Text_20: {
     fontSize: 15,
     backgroundColor: "#1d95c9",
@@ -103,15 +122,19 @@ const styles = StyleSheet.create({
     textAlign: "center"
   },
   View_35: { flexDirection: "row" },
-  View_36: { width: 100, height: 100 },
+  View_36: { width: 130, height: 100 },
   Image_45: { width: 50, height: 50 },
+  Text_55: {},
   View_37: { width: 170, height: 100 },
-  Image_48: { width: 50, height: 500 },
+  Image_138: { width: 50, height: 50 },
+  Text_102: {},
   View_41: { flexDirection: "row" },
-  View_42: { width: 173, height: 100 },
+  View_42: { width: 130, height: 100 },
   Image_50: { width: 50, height: 50 },
-  View_43: { width: 96, height: 100 },
-  Image_53: { width: 50, height: 50 }
+  Text_115: {},
+  View_43: { width: 170, height: 100 },
+  Image_53: { width: 50, height: 50 },
+  Text_134: {}
 })
 
 const mapStateToProps = state => {
