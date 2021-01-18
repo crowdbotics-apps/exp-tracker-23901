8574786f13b55bf51095d575d7b8ab95f61d41fa
Crowdbotics <team@crowdbotics.com>
2021-01-18 22:41:40 +0000

    Update components

--- src/features/BlankScreen3194269/screens/blank.js
+++ src/features/BlankScreen3194269/screens/blank.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { connect } from "react-redux"
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +11,65 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
-export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
-    return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { TextInput_14: "Modifier une d\xE9pense" }
+  }
+  static navigationOptions = ({ navigation }) => {
+    return { headerLeft: <SlideMenuIcon navigationProps={navigation} /> }
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View style={styles.View_3}>
+        <View style={styles.View_8}>
+          <View style={styles.View_9}>
+            <Image
+              source={{ uri: "https://via.placeholder.com/150" }}
+              style={styles.Image_12}
+            />
+            <TextInput
+              placeholder="Sample text input placeholder"
+              style={styles.TextInput_14}
+              value={this.state.TextInput_14}
+              onChangeText={nextValue =>
+                this.setState({ TextInput_14: nextValue })
+              }
+            />
+          </View>
+        </View>
+      </View>
+      <View style={styles.View_4}>
+        <View />
+        <View />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  View_3: { width: 300, height: 200 },
+  View_8: { flexDirection: "row" },
+  View_9: { width: 270, height: 100 },
+  Image_12: { width: 50, height: 50 },
+  TextInput_14: { width: "50%", height: 100 },
+  View_4: { flexDirection: "row" },
+  View_5: {},
+  View_6: {}
+})
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
